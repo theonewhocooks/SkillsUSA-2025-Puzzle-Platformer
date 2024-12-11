307530716b9035ee1999a29ad9463be5f6b918e0
Walter Hart-well White <113389560+theonewhocooks@users.noreply.github.com>
2024-12-11 09:14:50 -0800

    czczcczc

--- My project/Assets/david/Scripts/Levers.cs
+++ My project/Assets/david/Scripts/Levers.cs
@@ -5,16 +5,7 @@ using TMPro;
 
 public class Levers : MonoBehaviour
 {
-    public TMP_InputField charHolder;
-    public GameObject Lever1;
-    public GameObject Lever2;
-    public GameObject Lever3;
-    public GameObject Lever4;
-    public GameObject Lever5;
-    public GameObject Lever6;
-
-
-    void OnColliderEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player1"))
         {
@@ -23,50 +14,4 @@ public class Levers : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            charHolder.text = charHolder.text + "2";
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            charHolder.text = charHolder.text + "3";
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            charHolder.text = charHolder.text + "4";
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            charHolder.text = charHolder.text + "5";
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            charHolder.text = charHolder.text + "6";
-        }
-
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            charHolder.text = null;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if (charHolder.text == "42")
-            {
-                Debug.Log("yarito");
-            }
-            else
-            {
-                Debug.Log("don larma");
-            }
-
-            charHolder.text = null;
-        }
-    }
 }
