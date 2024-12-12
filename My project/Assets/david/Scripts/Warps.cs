91253767b567a757a929010f942ec3bb2644173f
Walter Hart-well White <davidneff469@riversideunified.org>
2024-12-11 20:58:28 -0800

    IM GONNA

--- My project/Assets/david/Scripts/Warps.cs
+++ My project/Assets/david/Scripts/Warps.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Levers : MonoBehaviour
 {
+    public Vector3 targetPosition;
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player1"))
         {
-            Debug.Log("putttttt it innnn my asssssss");
-
+            other.gameObject.transform.position = targetPosition;
         }
     }
-
 }
